e4a0c195d6bb5124153b844f42dd0c380e749fe4
Roman <romandulman@gmail.com>
2019-09-23 20:53:09 +0300

    Cleaned code

--- server/app.js
+++ server/app.js
@@ -1,37 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/// For Live Demo purposes only
+// This Node server runs only static React build files - not backend logic !
 
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
 
-var app = express();
-
+const app = express();
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
